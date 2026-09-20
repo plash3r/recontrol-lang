4e0999805a2fcf984223ef431a4081164cad4940
plash3r <plash3r@gmail.com>
2026-09-20 19:57:33 +0300

    installer: install bundled Linux runtime

--- install.sh
+++ install.sh
@@ -4,11 +4,13 @@ set -eu
 REPO="plash3r/recontrol-lang"
 INSTALL_DIR="${RCL_INSTALL_DIR:-$HOME/.local/bin}"
 ASSET="rcl-linux-x86_64"
+RUNTIME_ASSET="librcl_runtime.a"
 URL="https://github.com/$REPO/releases/latest/download/$ASSET"
 
 printf '%s\n' "Installing Recontrol Lang..."
 mkdir -p "$INSTALL_DIR"
 curl -fsSL "$URL" -o "$INSTALL_DIR/rcl"
+curl -fsSL "https://github.com/$REPO/releases/latest/download/$RUNTIME_ASSET" -o "$INSTALL_DIR/$RUNTIME_ASSET"
 chmod +x "$INSTALL_DIR/rcl"
 
 printf '%s\n' "Installed rcl to $INSTALL_DIR/rcl"
