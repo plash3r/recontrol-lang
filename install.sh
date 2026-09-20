4f1143be8c3f14e75e79b7f7fa73483950d5069d
plash3r <plash3r@gmail.com>
2026-09-20 20:45:07 +0300

    simplify Linux installer

--- install.sh
+++ install.sh
@@ -4,19 +4,12 @@ set -eu
 REPO="plash3r/recontrol-lang"
 INSTALL_DIR="${RCL_INSTALL_DIR:-$HOME/.local/bin}"
 ASSET="rcl-linux-x86_64"
-RUNTIME_ASSET="librcl_runtime.a"
-TOOLCHAIN_ASSET="rcl-toolchain-linux-x86_64.tar.gz"
 URL="https://github.com/$REPO/releases/latest/download/$ASSET"
 
 printf '%s\n' "Installing Recontrol Lang..."
 mkdir -p "$INSTALL_DIR"
 curl -fsSL "$URL" -o "$INSTALL_DIR/rcl"
-curl -fsSL "https://github.com/$REPO/releases/latest/download/$RUNTIME_ASSET" -o "$INSTALL_DIR/$RUNTIME_ASSET"
-tmp="$(mktemp -d)"
-trap 'rm -rf "$tmp"' EXIT
-curl -fsSL "https://github.com/$REPO/releases/latest/download/$TOOLCHAIN_ASSET" -o "$tmp/toolchain.tar.gz"
-tar -xzf "$tmp/toolchain.tar.gz" -C "$INSTALL_DIR"
-chmod +x "$INSTALL_DIR/rcl" "$INSTALL_DIR/rcl-toolchain/bin/llc" "$INSTALL_DIR/rcl-toolchain/bin/ld.lld"
+chmod +x "$INSTALL_DIR/rcl"
 
 printf '%s\n' "Installed rcl to $INSTALL_DIR/rcl"
 
