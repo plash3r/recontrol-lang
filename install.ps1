8a45df19c4e494e32809cefac09b458fc97a6bef
plash3r <plash3r@gmail.com>
2026-09-20 19:57:37 +0300

    installer: install bundled Windows runtime

--- install.ps1
+++ install.ps1
@@ -3,12 +3,14 @@ $ErrorActionPreference = "Stop"
 $repo = "plash3r/recontrol-lang"
 $installDir = if ($env:RCL_INSTALL_DIR) { $env:RCL_INSTALL_DIR } else { Join-Path $HOME ".rcl\bin" }
 $asset = "rcl-windows-x86_64.exe"
+$runtimeAsset = "rcl_runtime.lib"
 $url = "https://github.com/$repo/releases/latest/download/$asset"
 $target = Join-Path $installDir "rcl.exe"
 
 Write-Host "Installing Recontrol Lang..."
 New-Item -ItemType Directory -Force -Path $installDir | Out-Null
 Invoke-WebRequest -Uri $url -OutFile $target
+Invoke-WebRequest -Uri "https://github.com/$repo/releases/latest/download/$runtimeAsset" -OutFile (Join-Path $installDir $runtimeAsset)
 
 $userPath = [Environment]::GetEnvironmentVariable("Path", "User")
 if ($userPath -notlike "*$installDir*") {
