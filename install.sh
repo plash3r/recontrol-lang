0371bc83eb6ca74c9613446cd1a140e909ed4710
plash3r <plash3r@gmail.com>
2026-09-20 20:11:51 +0300

    installer: install bundled native toolchain

--- install.sh
+++ install.sh
@@ -5,13 +5,18 @@ REPO="plash3r/recontrol-lang"
 INSTALL_DIR="${RCL_INSTALL_DIR:-$HOME/.local/bin}"
 ASSET="rcl-linux-x86_64"
 RUNTIME_ASSET="librcl_runtime.a"
+TOOLCHAIN_ASSET="rcl-toolchain-linux-x86_64.tar.gz"
 URL="https://github.com/$REPO/releases/latest/download/$ASSET"
 
 printf '%s\n' "Installing Recontrol Lang..."
 mkdir -p "$INSTALL_DIR"
 curl -fsSL "$URL" -o "$INSTALL_DIR/rcl"
 curl -fsSL "https://github.com/$REPO/releases/latest/download/$RUNTIME_ASSET" -o "$INSTALL_DIR/$RUNTIME_ASSET"
-chmod +x "$INSTALL_DIR/rcl"
+tmp="$(mktemp -d)"
+trap 'rm -rf "$tmp"' EXIT
+curl -fsSL "https://github.com/$REPO/releases/latest/download/$TOOLCHAIN_ASSET" -o "$tmp/toolchain.tar.gz"
+tar -xzf "$tmp/toolchain.tar.gz" -C "$INSTALL_DIR"
+chmod +x "$INSTALL_DIR/rcl" "$INSTALL_DIR/rcl-toolchain/bin/clang" "$INSTALL_DIR/rcl-toolchain/bin/ld.lld"
 
 printf '%s\n' "Installed rcl to $INSTALL_DIR/rcl"
 
