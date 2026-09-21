c27c06fbc9fe8286002020114eaa076eca7c8727
plash3r <plash3r@gmail.com>
2026-09-21 05:51:40 +0300

    mir: handle enum rvalues in flow checks

--- compiler/src/mir_borrow.rs
+++ compiler/src/mir_borrow.rs
@@ -168,6 +168,8 @@ impl MirBorrowAnalyzer {
             Rvalue::Aggregate { fields, .. } => {
                 for (_, op) in fields { Self::check_operand(function, block, op, state, errors); }
             }
+            Rvalue::EnumVariant { .. } => {}
+            Rvalue::EnumTag { operand } => Self::check_operand(function, block, operand, state, errors),
             Rvalue::Array(values) => {
                 for op in values { Self::check_operand(function, block, op, state, errors); }
             }
