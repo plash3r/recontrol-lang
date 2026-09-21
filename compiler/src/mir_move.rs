de6f96eac12831197d8967a5ce57f111fb39df0d
plash3r <plash3r@gmail.com>
2026-09-21 16:05:25 +0300

    mir: analyze payload enum operands in flow checks

--- compiler/src/mir_move.rs
+++ compiler/src/mir_move.rs
@@ -194,8 +194,11 @@ impl MirMoveAnalyzer {
                     Self::check_operand(function, block, op, state, errors);
                 }
             }
-            Rvalue::EnumVariant { .. } => {}
-            Rvalue::EnumTag { operand } => Self::check_operand(function, block, operand, state, errors),
+            Rvalue::EnumVariant { values, .. } => {
+                for operand in values { Self::check_operand(function, block, operand, state, errors); }
+            }
+            Rvalue::EnumTag { operand } | Rvalue::EnumPayload { operand, .. } =>
+                Self::check_operand(function, block, operand, state, errors),
             Rvalue::Array(values) => {
                 for op in values {
                     Self::check_operand(function, block, op, state, errors);
